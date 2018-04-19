d00852b17369bbfc61567446fbc37b2e15494229
Vytis Šibonis <vytis@wire.com>
2018-04-19 11:59:50 +0200

    Make protocol objc

--- Source/Public/TearDownCapable.swift
+++ Source/Public/TearDownCapable.swift
@@ -20,7 +20,7 @@ import Foundation
 
 /// Object that can be torn down when not needed anymore
 @objc
-public protocol TearDownCapable {
+public protocol TearDownCapable: NSObjectProtocol {
     
     @objc
     func tearDown()
