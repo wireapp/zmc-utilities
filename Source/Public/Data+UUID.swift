feb8ae752bc996d2d0cd96a8fb509041b616b48b
Bill Chan <billypchan@gmail.com>
2019-05-07 10:53:40 +0200

    create UUID from uuit_t

--- Source/Public/Data+UUID.swift
+++ Source/Public/Data+UUID.swift
@@ -20,7 +20,26 @@ import Foundation
 
 extension Data {
     var toUUID: UUID? {
-        return NSUUID(uuidBytes: withUnsafeBytes(UnsafePointer<UInt8>.init)) as UUID?
+        let bytes = [UInt8](self)
+
+        let uuidTriple: uuid_t = (bytes[0],
+                                  bytes[1],
+                                  bytes[2],
+                                  bytes[3],
+                                  bytes[4],
+                                  bytes[5],
+                                  bytes[6],
+                                  bytes[7],
+                                  bytes[8],
+                                  bytes[9],
+                                  bytes[10],
+                                  bytes[11],
+                                  bytes[12],
+                                  bytes[13],
+                                  bytes[14],
+                                  bytes[15])
+
+        return UUID(uuid: uuidTriple)
     }
 }
 
