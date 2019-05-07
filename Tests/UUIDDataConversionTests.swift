75abb800a61d160c1e89498bcbdc8d5e19fcef0c
Bill Chan <billypchan@gmail.com>
2019-05-07 11:22:21 +0200

    more invalid cases

--- Tests/UUIDDataConversionTests.swift
+++ Tests/UUIDDataConversionTests.swift
@@ -22,32 +22,34 @@ import XCTest
 
 final class UUIDDataConversionTests: XCTestCase {
     func testThatUUIDisConvertedToData() {
+        //GIVEN
         let uuid = UUID(uuidString: "00010203-0405-0607-0809-0a0b0c0d0e0f")
 
+        //WHEN
         let data = uuid?.toData
 
         XCTAssertEqual(data?.count, 16)
 
+        //THEN
         let bytes = [UInt8](data!)
         XCTAssertEqual(bytes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
     }
 
     func testThatDataisConvertedToUUID() {
+        //GIVEN
         let bytes: [UInt8] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
         let data = Data(bytes: bytes)
 
+        //WHEN
         let uuid = data.toUUID
 
+        //THEN
         XCTAssertEqual(uuid, UUID(uuidString: "00010203-0405-0607-0809-0a0b0c0d0e0f"))
     }
 
 
     func testThatDataWithIncorrectLengthIsNotConverted() {
-        let bytes: [UInt8] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
-        let data = Data(bytes: bytes)
-
-        let uuid = data.toUUID
-
-        XCTAssertNil(uuid)
+        XCTAssertNil(Data(bytes: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]).toUUID)
+        XCTAssertNil(Data(bytes: [0,1,2]).toUUID)
     }
 }
